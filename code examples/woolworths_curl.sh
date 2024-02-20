ba5c7f277abe59e7d76aaaf0561b1889bc6ccc8c
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:39:43 +0200

    Adding code examples/woolworths_curl.sh

---
+++ code examples/woolworths_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.woolworths.com.au/shop/browse/specials/online-only-specials?icmpid=sm-prnav-sc-onlineonlyspecials"}'
