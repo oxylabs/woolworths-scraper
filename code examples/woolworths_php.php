1b352a5ddd9fc3a19f020ee354df5f0f4b180266
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:39:41 +0200

    Adding code examples/woolworths_php.php

---
+++ code examples/woolworths_php.php
@@ -0,0 +1,27 @@
+<?php
+
+$params = [
+    'source' => 'universal_ecommerce',
+    'url' => 'https://www.woolworths.com.au/shop/browse/specials/online-only-specials?icmpid=sm-prnav-sc-onlineonlyspecials',
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "user" . ":" . "pass1");
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
